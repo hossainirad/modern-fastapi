cbf5c8fc298d7bc1b159336b3f87a83fceef372a
ali <ali.hossaini34@yahoo.com>
2022-03-29 21:43:50 +0430

    some more organization

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ from api import weather_api
 from views import home
 
 api = fastapi.FastAPI()
-api.mount('/static', StaticFiles(directory='static'), name='static')
-api.include_router(home.router)
-api.include_router(weather_api.router)
+
+def configure():
+    configure_routing()
+
+def configure_routing():
+    api.mount('/static', StaticFiles(directory='static'), name='static')
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
 
 if __name__ == '__main__':
+    configure()
     uvicorn.run(api)
+else:
+    configure()
